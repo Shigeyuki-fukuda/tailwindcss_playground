7d3afb0afa36ec7ec7b84b460f520272edeec45b
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-05 22:16:05 +0900

    feat: freshという名前でオリジナルのcolorを追加し、npm run build

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-blue': colors.lightBlue
+        'light-blue': colors.lightBlue,
+        'rose': {
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          200: '#fecdd3',
+          300: '#fda4af',
+          400: '#fb7185',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+          800: '#9f1239',
+          900: '#881337',
+        },
+        'fresh': {
+          // https://www.schemecolor.com/cool-and-fresh.php
+          100: '#D6FEC1',
+          200: '#AFFFB5',
+          300: '#EFFFFB',
+          400: '#C4FFFF',
+          500: '#AFE8FF',
+          600: '#ACFFD4',
+        }
       }
     },
   },
